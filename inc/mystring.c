85bbc953e675bcee54a4906bebbd50a418820cbc
99003674 <78849860+99003674@users.noreply.github.com>
2021-03-04 00:02:10 +0530

    Create mystring.c

---
+++ inc/mystring.c
@@ -0,0 +1,12 @@
+#ifndef __MYSTRING_H_
+#define __MYSTRING_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+char mystrcpy(char,char);
+int mystrlen(char);
+char mystrcat(char,char);
+int mystrcmp(char,char);
+
+
+#endif 
